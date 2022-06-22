c5e4d52f05133173130d54a62290fa5673cb6032
Kerim <Kerim@DESKTOP-JDQ2UQC>
2022-06-23 01:09:06 +0400

    Добавьте файлы проекта.

---
+++ EFBuyers/Buyer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFBuyers
+{
+    public class Buyer
+    {
+        public string? FullName { get; set; } = null!;
+        public string? Gender {get; set;} = null!;
+        public string? Email {get; set;} = null!;
+        public string? Country { get; set;} = null!;
+
+        public string? City { get; set; } = null!;
+        public string? Section { get; set;} = null!;
+
+        public override string ToString()
+        {
+            return $"Full Name: {FullName}\n" +
+                   $"Gender: {Gender}\n" +
+                   $"Email: {Email}\n" +
+                   $"Country: {Country}\n" +
+                   $"City: {City}\n" +
+                   $"Section: {Section}";
+                    
+
+        }
+    }
+}
---
+++ EFBuyers/Program.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using EFBuyers;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+IDbConnection conn = new SqlConnection("Data Source=DESKTOP-JDQ2UQC;Initial Catalog=Stocks;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); 
+
+//2
+try
+{
+    conn.Open();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Connection false");
+}
+Console.WriteLine("Connection success");
+
+//3
+string query = "select * from Buyer";
+
+var result = conn.Query<Buyer>(query).ToList();
+
+/*foreach (var item in result)
+{
+    Console.WriteLine(item+"\n");
+}
+conn.Close();*/
+
+//4
+
+result = conn.Query<Buyer>(ByCity("Sumgayit")).ToList();
+
+foreach (var item in result)
+{
+    Console.WriteLine(item + "\n");
+}
+conn.Close();
+
+string ByCountry(string _countryname)
+{
+    string query = $"select * from Buyer where Country = '{_countryname}'";
+
+    return query;
+}
+
+string ByCity(string _cityname)
+{
+    string query = $"select * from Buyer where City = '{_cityname}'";
+
+    return query;
+}
+
+string BySection(string _section)
+{
+    string query = $"select * from Buyer where Section = '{_section}'";
+
+    return query;
+}
+
